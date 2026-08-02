a7a587a8026dd59a8ccfa6766a8ec3a92c2a8b9d
qjp <3170773676@qq.com>
2026-08-02 23:41:11 +0800

    fix: 更新__init__文件

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,13 @@
-"""
-@Author: QntmJP
-@Desc: 
-"""
+"""API 路由模块"""
+
+from .health import router as health_router
+from .chat import router as chat_router
+from .aiops import router as aiops_router
+from .file import router as file_router
+
+__all__ = [
+    "health_router",
+    "chat_router",
+    "aiops_router",
+    "file_router",
+]
